aa23032cd5c620a551a88a212269bdec905ed57d
Alex Kellas <alex.kellas.relton@gmail.com>
2025-11-12 14:49:10 +0000

    app made, and Explorer page set at home

--- SolarSystemExplorer.Client/Program.cs
+++ SolarSystemExplorer.Client/Program.cs
@@ -1,7 +1,13 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
+using SolarSystemExplorer.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+
+builder.RootComponents.Add<App>("#app");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddMudServices();
 builder.Services.AddMudBlazorDialog();
