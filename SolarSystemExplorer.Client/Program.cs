77fa1b70073e6fc91378b9ae4e19968c6bada165
Alex Kellas <alex.kellas.relton@gmail.com>
2025-11-12 14:20:04 +0000

    MudBlazor added to project

--- SolarSystemExplorer.Client/Program.cs
+++ SolarSystemExplorer.Client/Program.cs
@@ -1,5 +1,11 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddMudServices();
+builder.Services.AddMudBlazorDialog();
+builder.Services.AddMudBlazorSnackbar();
+builder.Services.AddMudBlazorResizeListener();
 
 await builder.Build().RunAsync();
